d41c2f4e84a402aadc6ca594269cb511a913f6d2
Matysekxx <chaloupkamatej1353@gmail.com>
2026-02-12 10:40:29 +0100

    Add struct Variable

---
+++ core/Variable.h
@@ -0,0 +1,14 @@
+//
+// Created by chalo on 11.02.2026.
+//
+
+#ifndef VARIABLE_H
+#define VARIABLE_H
+#include "Value.h"
+
+struct Variable {
+    Value value;
+    bool isMutable;
+};
+
+#endif //VARIABLE_H
